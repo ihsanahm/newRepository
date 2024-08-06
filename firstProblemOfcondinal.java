37c07ce7144c3e946b9c86c9a7fff2db293f8f48
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2024-08-06 11:23:30 +0500

    Upadate the problem

--- firstProblemOfcondinal.java
+++ firstProblemOfcondinal.java
@@ -4,8 +4,17 @@ import java.util.Scanner;
  public class firstProblemOfcondinal{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number :");
-        int num=sc.nextInt();
-        System.out.println("The number is :"+num);
+        System.out.println("** Welcome to the Driving license portal ** ");
+        System.out.print("Enter Your Age :");
+        int age=sc.nextInt();
+        if(age>=18)
+        {
+            System.out.println("You are eligible for driving license");
+        }
+        else
+        {
+            System.out.println("You are not eligible for driving license");
+        }
+        System.out.println("Thank you for using our portal.");
     }
 }
